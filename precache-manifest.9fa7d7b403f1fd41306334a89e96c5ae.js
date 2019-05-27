3cb25c2a8a75f695b9a1de029a6fd01f7b744f1e
cnrtsvr <caneratasever@gmail.com>
2019-05-27 16:45:33 +0300

    Updates

---
+++ precache-manifest.9fa7d7b403f1fd41306334a89e96c5ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21401c3510e6825a0602",
+    "url": "/task_textkernel/static/css/main.294bf3cc.chunk.css"
+  },
+  {
+    "revision": "21401c3510e6825a0602",
+    "url": "/task_textkernel/static/js/main.dc9c6aea.chunk.js"
+  },
+  {
+    "revision": "451801f594cbac4562ac",
+    "url": "/task_textkernel/static/js/runtime~main.601637d8.js"
+  },
+  {
+    "revision": "8301c1c77e683941c8f0",
+    "url": "/task_textkernel/static/js/2.cb6a12d4.chunk.js"
+  },
+  {
+    "revision": "6005bdefc29c50ba024600f3c5992217",
+    "url": "/task_textkernel/index.html"
+  }
+];
